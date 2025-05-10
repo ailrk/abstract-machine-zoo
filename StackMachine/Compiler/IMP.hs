f6a3e9c160a93919b0cd6c89b1bf4324b495d58a
Ailrk <jimmy123good@gmail.com>
2025-05-09 21:52:37 -0700

    Add debug

--- StackMachine/Compiler/IMP.hs
+++ StackMachine/Compiler/IMP.hs
@@ -4,7 +4,6 @@ import Machine
 import qualified IMP
 import Prelude hiding (LT, GT)
 import Control.Monad.State
-import Debug.Trace
 
 
 compile :: IMP.Stmt -> Program
@@ -16,7 +15,7 @@ compileStmt stmt =
   case stmt of
     IMP.Assign n e -> do
       eprog <- compileExpr e
-      store <- emit [STORE n]
+      store <- emit (STORE n)
       combine
         [ eprog
         , store
@@ -24,10 +23,12 @@ compileStmt stmt =
     IMP.Seq stmts -> mconcat <$> traverse compileStmt stmts
     IMP.If c t e -> do
       cprog <- compileExpr c
+      compensate 1 -- compensate cond
       tprog@(Program(tinstrs, _)) <- compileStmt t
+      compensate 1 -- compensate pass
       eprog@(Program(einstrs, _)) <- compileStmt e
-      cond <- emit [ JZ (length tinstrs + 2) ]
-      pass <- emit [ JUMP (length einstrs + 1) ]
+      let cond = fromInstr (JZ (length tinstrs + 2))
+      let pass = fromInstr (JUMP (length einstrs + 1))
       combine
         [ cprog
         , cond
@@ -37,9 +38,11 @@ compileStmt stmt =
         ]
     IMP.While c s -> do
       cprog@(Program (cinstrs, _)) <- compileExpr c
+      compensate 1 -- compensate cond
       sprog@(Program(sinstrs, _)) <- compileStmt s
-      cond <- emit [JZ (length sinstrs + 2)]
-      back <- emit [JUMP (negate (length sinstrs + length cinstrs + 1))]
+      compensate 1 -- compensate back
+      let cond = fromInstr (JZ (length sinstrs + 2))
+      let back = fromInstr (JUMP (negate (length sinstrs + length cinstrs + 1)))
       combine
         [ cprog
         , cond
@@ -48,17 +51,18 @@ compileStmt stmt =
         ]
     IMP.Call f args -> do
       argsProg <- mconcat <$> traverse compileExpr args
-      call <- emit [CALL f]
+      call <- emit (CALL f)
       combine [ argsProg, call ]
     IMP.FuncDef n args body -> do
-      line <- get <* compensate 1
-      save <- emit $ fmap STORE (reverse args)
+      line <- get
+      compensate 1 -- need to compensate for `skip`
+      save <- mconcat <$> traverse (emit . STORE) (reverse args)
       bodyProg <- compileStmt body
-      ret <- emit [RETURN]
+      ret <- emit RETURN
       line' <- get
-      let skip = Program ([JUMP (line' - line)], mempty)
+      let skip = fromInstr (JUMP (line' - line))
       combine $
-        [ skip -- skip function body, only way to execute function is to call
+        [ skip -- if fall through simply skip the function body
         , save
         , bodyProg
         , ret
@@ -66,7 +70,7 @@ compileStmt stmt =
         ]
     IMP.Print e -> do
       exprProg <- compileExpr e
-      p <- emit [PRINT]
+      p <- emit PRINT
       combine
         [ exprProg
         , p
@@ -76,8 +80,8 @@ compileStmt stmt =
 compileExpr :: IMP.Expr -> State Int Program
 compileExpr expr =
   case expr of
-    IMP.Var x -> emit [LOAD x]
-    IMP.IntLit n -> emit [PUSH n]
+    IMP.Var x -> emit (LOAD x)
+    IMP.IntLit n -> emit (PUSH n)
     IMP.BinOP op a b -> do
       aProg <- compileExpr a
       bProg <- compileExpr b
@@ -93,28 +97,32 @@ compileExpr expr =
 compileOp :: IMP.OP -> State Int Program
 compileOp op =
   case op of
-    IMP.Add -> emit [ADD]
-    IMP.Sub -> emit [SUB]
-    IMP.Mul -> emit [MUL]
-    IMP.Div -> emit [DIV]
-    IMP.GT -> emit [GT]
-    IMP.GTE -> emit [GTE]
-    IMP.LT -> emit [LT]
-    IMP.LTE -> emit [LTE]
-    IMP.NOT -> emit [NOT]
+    IMP.Add -> emit ADD
+    IMP.Sub -> emit SUB
+    IMP.Mul -> emit MUL
+    IMP.Div -> emit DIV
+    IMP.GT -> emit GT
+    IMP.GTE -> emit GTE
+    IMP.LT -> emit LT
+    IMP.LTE -> emit LTE
+    IMP.NOT -> emit NOT
 
 
 addLabel :: Label -> Program
 addLabel label = (Program ([], pure label))
 
 
-
 combine :: [Program] -> State Int Program
 combine = pure . mconcat
 
 
-emit :: [Instr] -> State Int Program
-emit instrs = modify (+ (length instrs)) >> pure (Program (instrs, mempty))
+-- | Emit an instruction and line number
+emit :: Instr -> State Int Program
+emit instr = modify (+ 1) >> pure (Program ([instr], mempty))
+
+
+fromInstr :: Instr -> Program
+fromInstr instr = Program ([instr], mempty)
 
 
 compensate :: Int -> State Int ()
--- StackMachine/Main.hs
+++ StackMachine/Main.hs
@@ -1,20 +1,38 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main where
 
 import qualified IMP
 import qualified Compiler.IMP
 import qualified Machine
 import System.Environment (getArgs)
+import Data.Foldable (traverse_)
 
 
-eval :: IMP.Stmt -> IO ()
-eval stmt = do
+data Mode
+  = Normal
+  | Debug
+
+debug :: Machine.Program -> IO ()
+debug program = do
+  let (Machine.Program (instrs, labels)) = program
+  print labels
+  traverse_ print (zip [0..] instrs)
+
+
+eval :: Mode -> IMP.Stmt -> IO ()
+eval mode stmt = do
   let program = Compiler.IMP.compile stmt
+  case mode of
+    Debug -> debug program
+    Normal -> pure ()
   Machine.start program
 
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    path:_ -> IMP.run path eval
-    _ -> fail "Usage: stack-machine <filename>"
+  let mode = if "--debug" `elem` args then Debug else Normal
+  let stripped = filter (/= "--debug") args
+  case stripped of
+    path:_ -> IMP.run path (eval mode)
+    _ -> fail "Usage: stack-machine <filename> [--debug]"
