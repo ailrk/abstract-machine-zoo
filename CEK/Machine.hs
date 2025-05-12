46832eabf06aaff4568b10299e70d38588d01e01
Ailrk <jimmy123good@gmail.com>
2025-05-12 11:18:59 -0700

    Add CEK

--- CEK/Machine.hs
+++ CEK/Machine.hs
@@ -1,1 +1,34 @@
 module Machine where
+import Data.Text (Text)
+import qualified LC
+
+
+type Symbol = Text
+type Env = [(Symbol, Closure)]
+data Closure = Closure Core Env deriving (Eq, Show)
+
+
+data Core
+  = Var Text
+  | Abs Text Core
+  | App Core Core
+  -- builtins
+  | IntLit Int
+  | Op LC.OP -- All operators are curried
+  deriving (Eq, Show)
+
+
+-- Feature if CEK is explicit continuation
+data Kont
+  = Empty            -- empty continuation
+  | Ar Core Env Kont -- argument frame
+  | Fn Closure Kont  -- function frame
+  deriving (Eq, Show)
+
+
+data CEK = CEK
+  { _expr :: Core -- (C)ontrol
+  , _env :: Env   -- (E)nvironment
+  , _cont :: Kont -- (K)ontinuation
+  }
+  deriving (Eq, Show)
